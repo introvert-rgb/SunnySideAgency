2e050f6955a036456cb9dd0fc1bf38f89084b25a
Anup Singh <anups5246@gmail.com>
2022-05-07 13:19:11 +0530

    animation

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
+let icons = document.querySelectorAll('.sm-icon');
+let target = document.querySelectorAll('.svg-holder');
+let tl = gsap.timeline({ defaults: { Easings: Expo.EaseOut } });
+
 $(document).ready(() => {
     $(".menuBtn").click(() => {
         $(".menu").toggleClass('hide');
 
+    });
+
+});
+icons.forEach((icon) => {
+    icon.addEventListener('mouseenter', () => {
+        icon.setAttribute('fill', 'white');
     })
-})
+    icon.addEventListener('mouseleave', () => {
+        icon.setAttribute('fill', '#2C7566');
+    })
+});
+tl.from('.section-01', { scale: 0.9, duration: 2, opacity: 0, ease: Expo.EaseOut, delay: 0.2 })
+    .from('.section-inner', {
+            scale: 0.5,
+            opacity: 0
+        },
+        '-=1').from('.main-content', { clipPath: ' polygon(100% 100%, 100% 0, 100% 100%, 0% 100%)', y: 100 })
